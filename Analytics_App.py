0c013a5d10fc1a9f9200b6f342597908ba31cb5c
harashitmitra-oss <harashit.mitra@tetr.org>
2026-02-27 19:19:14 +0530

    Update Analytics_App.py

--- Analytics_App.py
+++ Analytics_App.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 # =========================
 # Page config
 # =========================
-st.set_page_config(page_title="Engagement Analytics Dashboard", layout="wide")
+st.set_page_config(page_title="Tetr Engagement Analytics Dashboard", layout="wide")
 
 
 # =========================
